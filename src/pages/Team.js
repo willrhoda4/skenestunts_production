9b5b71ce389c52da403bbe620a7bb8ffc9897f1d
willrhoda4 <willrhoda4@gmail.com>
2023-05-25 22:36:49 -0500

    implementing branding

--- src/pages/Team.js
+++ src/pages/Team.js
@@ -108,6 +108,18 @@ export default function Team({teamData, teamPosters, boardData, boardPosters}) {
               { attributes() }
             </div>
 
+
+             {/* stripes and logo are absolutely-positoned background elements */}
+          <div className='boardProfileStripes' />
+
+            <Picture
+                      src={logo}
+                fallback={logoPng}
+                    type='image/webp'
+                      alt='Skene Stunts company logo'
+                className='boardProfileLogo'
+            />
+
           </div>
           
           <p className='boardProfileBlurb' >{profile}</p>
@@ -123,16 +135,7 @@ export default function Team({teamData, teamPosters, boardData, boardPosters}) {
 
 
 
-         {/* stripes and logo are absolutely-positoned background elements */}
-         <div className='boardProfileStripes' />
-
-          <Picture
-                    src={logo}
-              fallback={logoPng}
-                  type='image/webp'
-                    alt='Skene Stunts company logo'
-              className='boardProfileLogo'
-          />
+        
 
 
 
