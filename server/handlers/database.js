a2d11100eadc5e6a4fc6e533add5846ac6010f7e
willrhoda4 <willrhoda4@gmail.com>
2023-05-25 20:32:11 -0500

    implementing branding

--- server/handlers/database.js
+++ server/handlers/database.js
@@ -11,21 +11,21 @@ const Pool = require('pg').Pool
 
 
 //                                      vvvvvv use this configuration for local development
-const pool = new Pool({
-                        user:     process.env.PG_USER,
-                        host:     process.env.PG_HOST,
-                        database: process.env.PG_DATABASE,
-                        password: process.env.PG_PASSWORD,
-                        port:     process.env.PG_PORT,
-                        ssl:      true
-                      })
+// const pool = new Pool({
+//                         user:     process.env.PG_USER,
+//                         host:     process.env.PG_HOST,
+//                         database: process.env.PG_DATABASE,
+//                         password: process.env.PG_PASSWORD,
+//                         port:     process.env.PG_PORT,
+//                         ssl:      true
+//                       })
 
 
 // use this configuration for deployment to Heroku
-// const pool = new Pool({
-//                         connectionString: process.env.DATABASE_URL,
-//                         ssl:            { rejectUnauthorized: false }
-//                       });
+const pool = new Pool({
+                        connectionString: process.env.DATABASE_URL,
+                        ssl:            { rejectUnauthorized: false }
+                      });
 
 
 
