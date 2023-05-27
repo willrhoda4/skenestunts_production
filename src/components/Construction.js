b24fcefd688c28720e030da0ee0c50052c96aaad
willrhoda4 <willrhoda4@gmail.com>
2023-05-27 11:05:04 -0500

    starting final tweaks

--- src/components/Construction.js
+++ src/components/Construction.js
@@ -20,13 +20,24 @@ import logoPng  from                '../images/logo_error.png';
 // this component is a placeholder for when the site is down for maintenance.
 // it's a simple page with a logo and a message.
 // and can be summoned through the Director's Chair at the admin's bequest.
-export default function Construction () {
+export default function Construction ({errorBoundary}) {
+
+
+    const heading = errorBoundary ? 'Looks like someting went wrong...'
+                                  : 'we\'re doing some maintenance...';
+
+    const apology = errorBoundary ? 'Sorry about this. Try refreshing the browser, and if the problem persists send us an email.'
+                                  : 'Sorry for the inconvenience. We\'re doing our best to keep the downtime short, so be sure to check back soon.';
+
+  
+    
+
 
     return(
 
         <div className='constructionPage'>
             
-            <h2>we're doing some maintenance...</h2>
+            <h2>{heading}</h2>
 
             <Picture
                          src={logo}
@@ -36,7 +47,7 @@ export default function Construction () {
                           id='errorLogo'
             />
 
-            <p className='constructionGraf'>Sorry for the inconvenience. We're doing our best to keep the downtime short, so be sure to check back soon.</p>
+            <p className='constructionGraf'>{apology}</p>
 
         </div>
     )
