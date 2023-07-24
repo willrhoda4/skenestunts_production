caf55631ec026f33a6d03fcc07a9a0d3c8849215
willrhoda4 <willrhoda4@gmail.com>
2023-07-24 15:35:47 -0500

    tweaking info social

--- src/components/Picture.js
+++ src/components/Picture.js
@@ -17,24 +17,26 @@ export default function Picture({ src, type, fallback, id, className, alt, style
                  className={className}
                      style={style} />
         
-      // <picture>
+              // this configuration may become relevant in the future,
+              // but for now it just causes problems with Tor browser.
+              // <picture>
 
-      //   <source  srcSet={src} 
-      //               type={type} 
-      //                 id={id} 
-      //         className={className}
-      //             style={style}
-      //               alt={alt} 
-      //   />
+              //   <source  srcSet={src} 
+              //               type={type} 
+              //                 id={id} 
+              //         className={className}
+              //             style={style}
+              //               alt={alt} 
+              //   />
 
-      //   <img        src={fallback} 
-      //                 id={id} 
-      //         className={className}
-      //             style={style} 
-      //               alt={alt} 
-      //   />
+              //   <img        src={fallback} 
+              //                 id={id} 
+              //         className={className}
+              //             style={style} 
+              //               alt={alt} 
+              //   />
 
-      // </picture>
+              // </picture>
 
     );
   }
