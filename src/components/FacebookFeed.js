685bac9da82efbe58376606b88a1aeacdca9d028
willrhoda4 <willrhoda4@gmail.com>
2023-08-07 18:03:04 -0500

    finalfinalfinal touch ups

--- src/components/FacebookFeed.js
+++ src/components/FacebookFeed.js
@@ -19,20 +19,20 @@ export default function FacebookFeed ({name, url}) {
 
 
     // injects the facebook sdk script into the DOM when the component mounts.
-    useEffect(() => {
+    // useEffect(() => {
 
-        const script = document.createElement('script');
-              script.src='https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v15.0&appId=1229119547492444&autoLogAppEvents=1';
-              script.crossorigin='anonymous';
-              script.nonce='DSHjCoBi';
-              script.async = true;
-              script.defer = true;                      
+    //     const script = document.createElement('script');
+    //           script.src='https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v15.0&appId=1229119547492444&autoLogAppEvents=1';
+    //           script.crossorigin='anonymous';
+    //           script.nonce='DSHjCoBi';
+    //           script.async = true;
+    //           script.defer = true;                      
                         
-        document.body.prepend(script);
+    //     document.body.prepend(script);
 
-        return () => { document.body.removeChild(script); }
+    //     return () => { document.body.removeChild(script); }
 
-    });
+    // }, [name, url]);
 
 
 
