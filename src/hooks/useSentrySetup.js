daa12742f49f364a28a793ac4822f298f808b116
William Rhoda <willrhoda4@gmail.com>
2024-11-07 12:23:19 -0600

    activating Sentry

--- src/hooks/useSentrySetup.js
+++ src/hooks/useSentrySetup.js
@@ -2,22 +2,25 @@
 
 
 
-
-
-
-import   * as Sentry      from "@sentry/react";
-import { BrowserTracing } from "@sentry/tracing";  
-import { Replay         } from "@sentry/replay";  
-
+  
+  
+  
+  
+  import   * as Sentry      from "@sentry/react";
+  import { BrowserTracing } from "@sentry/tracing";  
+  import { Replay         } from "@sentry/replay";  
+  import { useRef         } from 'react';
+  
 
 // variable to check if Sentry has already been initialized
-let isSentryInitialized = false;
 
 // this hook initializes Sentry with the provided DSN and sets up the integrations.
 export function useSentrySetup() {
-
+  
+  const isSentryInitializedRef = useRef(false);
+  
   // ensure Sentry is initialized only once
-  if (isSentryInitialized) return;
+  if (isSentryInitializedRef.current) return;
 
   console.log('\ninitializing sentry\n')
 
@@ -34,6 +37,6 @@ export function useSentrySetup() {
   });
 
   // set flag to true after initialization
-  isSentryInitialized = true;
+  isSentryInitializedRef.current = true;
 
 }
